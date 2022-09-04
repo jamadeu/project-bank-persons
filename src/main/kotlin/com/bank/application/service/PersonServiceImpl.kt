b57e988d7db82c65635a4e8a56fe8a5669a9e241
Jean Amadeu <jean.amadeu2@gmail.com>
2022-09-04 18:48:07 -0300

    update person service

--- src/main/kotlin/com/bank/application/service/PersonServiceImpl.kt
+++ src/main/kotlin/com/bank/application/service/PersonServiceImpl.kt
@@ -1,8 +1,26 @@
 package com.bank.application.service
 
+import com.bank.domain.model.Person
 import com.bank.domain.repository.PersonRepository
 import com.bank.domain.service.PersonService
 import jakarta.inject.Singleton
+import org.bson.types.ObjectId
 
 @Singleton
-class PersonServiceImpl(override val personRepository: PersonRepository) : PersonService(personRepository)
+class PersonServiceImpl(private val personRepository: PersonRepository) : PersonService {
+    override fun findById(id: ObjectId): Person? {
+        return personRepository.findById(id)
+    }
+
+    override fun save(person: Person): Person {
+        return personRepository.save(person)
+    }
+
+    override fun update(person: Person): Person {
+        return personRepository.save(person)
+    }
+
+    override fun deleteById(id: ObjectId) {
+        personRepository.deleteById(id)
+    }
+}
--- src/main/kotlin/com/bank/domain/service/PersonService.kt
+++ src/main/kotlin/com/bank/domain/service/PersonService.kt
@@ -1,27 +1,14 @@
 package com.bank.domain.service
 
 import com.bank.domain.model.Person
-import com.bank.domain.repository.PersonRepository
-import jakarta.inject.Singleton
 import org.bson.types.ObjectId
 
+interface PersonService {
+    fun findById(id: ObjectId): Person?
 
-open class PersonService(
-    open val personRepository: PersonRepository
-) {
-    fun findById(id: ObjectId): Person? {
-        return personRepository.findById(id)
-    }
+    fun save(person: Person): Person
 
-    fun save(person: Person): Person{
-        return personRepository.save(person)
-    }
+    fun update(person: Person): Person
 
-    fun update(person: Person): Person{
-        return personRepository.update(person)
-    }
-
-    fun deleteById(id: ObjectId){
-        personRepository.deleteById(id)
-    }
+    fun deleteById(id: ObjectId)
 }
