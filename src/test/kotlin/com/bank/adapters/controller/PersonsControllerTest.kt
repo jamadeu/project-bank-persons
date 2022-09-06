426ea4a9790d18ba84817107b50ca59dab718ae7
Jean Amadeu <jean.amadeu2@gmail.com>
2022-09-06 06:14:44 -0300

    tests findById

--- src/test/kotlin/com/bank/adapters/controller/PersonsControllerTest.kt
+++ src/test/kotlin/com/bank/adapters/controller/PersonsControllerTest.kt
@@ -1,5 +1,7 @@
 package com.bank.adapters.controller
 
+import com.bank.adapters.controller.dto.CreatePersonRequest
+import com.bank.adapters.controller.dto.FindPersonByIdResponse
 import com.bank.adapters.repository.MicronautDataRepository
 import com.bank.domain.model.Person
 import io.micronaut.http.HttpRequest
@@ -10,14 +12,13 @@ import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import io.micronaut.test.support.TestPropertyProvider
 import jakarta.inject.Inject
-import org.bson.types.ObjectId
 import org.junit.jupiter.api.*
 import org.testcontainers.containers.MongoDBContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
-import java.time.LocalDate
 import java.util.*
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 @Testcontainers
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -34,7 +35,7 @@ internal class PersonsControllerTest : TestPropertyProvider {
     @Container
     private val mongoDBContainer: MongoDBContainer =
         MongoDBContainer(DockerImageName.parse("mongo:4.0.10"))
-            .apply { start()  }
+            .apply { start() }
 
     @BeforeEach
     fun setup() {
@@ -66,16 +67,33 @@ internal class PersonsControllerTest : TestPropertyProvider {
     @Test
     fun `findById must return a person when success`() {
         val person = getPerson()
-        person.id = ObjectId("63169bc2cd0867bf1507a62d")
-        person.createdAt = LocalDate.now()
-        person.updatedAt = LocalDate.now()
-        val savedPerson = micronautDataRepository.save(person)
-        val response = client.toBlocking().exchange<Unit, String>(
-            HttpRequest.GET("/persons/${savedPerson.id}")
+        val id = getSavedPersonId(person)
+        val response = client.toBlocking().exchange<Unit, FindPersonByIdResponse>(
+            HttpRequest.GET("/persons/$id"), FindPersonByIdResponse::class.java
         )
 
         assert(HttpStatus.OK == response.status)
-//        assert(micronautDataRepository.findAll().count() == 0)
+        assert(response.body.isPresent)
+        with(response.body.get()){
+            assert(this.id == id)
+            assert(this.name == person.name)
+            assert(this.cpf == person.cpf)
+            assert(this.address == person.address)
+        }
+        assert(micronautDataRepository.findAll().count() == 1)
+    }
+
+    private fun getSavedPersonId(personToCreate: Person): String {
+        val createPersonRequest = CreatePersonRequest(
+            name = personToCreate.name,
+            cpf = personToCreate.cpf,
+            address = personToCreate.address
+        )
+        return client
+            .toBlocking()
+            .exchange<CreatePersonRequest, String>(
+                HttpRequest.POST("/persons", createPersonRequest)
+            ).header("location")!!
     }
 
     private fun getPerson(
